689fec8fa8840f765183942822f1efcce3753d50
ohnoyuuki <k024g0074@g.neec.ac.jp>
2026-09-06 18:41:07 +0900

    ｔ

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ TitleScene* titleScene = nullptr;
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// エンジンの初期化
-	KamataEngine::Initialize(L"壁壊す");
+	KamataEngine::Initialize(L"3064_ルームリンカー");
 
 	// DirectXCommon*インスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
